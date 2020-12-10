4b405ad02ea8d29a5976cd3d71473670a8506cce
Adrian <aaguero64@hotmail.com>
2020-12-10 15:06:28 -0800

    Menu changes

--- application/ApplicationCar.java
+++ application/ApplicationCar.java
@@ -2,7 +2,6 @@ package application;
 
 public class ApplicationCar {
 	public static void main(String[] args) { // Main method
-		System.out.println("Repository test");
 		MenuCar menu = new MenuCar();
 		menu.enter();
 	}
