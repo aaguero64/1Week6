08cc7abb028d5fd1f73941c029170f51f28912b7
Sri Srinivasan <sri.dhcs@gmail.com>
2020-12-10 15:25:56 -0800

    I changed my sysout and pushing to Adrian Branch

--- application/ApplicationCar.java
+++ application/ApplicationCar.java
@@ -3,7 +3,7 @@ package application;
 public class ApplicationCar {
 	public static void main(String[] args) { // Main method
 		MenuCar menu = new MenuCar();
-		System.out.println("Test Line from Sri");
+		System.out.println("Adrian, this is my test to your branch);
 		menu.enter();
 	}
 }
