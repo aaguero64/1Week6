b0ea36c61e49866d03f544e8e487f65cfe4f1f7b
Sri Srinivasan <sri.dhcs@gmail.com>
2020-12-10 15:58:30 -0800

    Another change and push to Adrian Branch

--- application/ApplicationCar.java
+++ application/ApplicationCar.java
@@ -4,6 +4,7 @@ public class ApplicationCar {
 	public static void main(String[] args) { // Main method
 		MenuCar menu = new MenuCar();
 		System.out.println("Adrian, this is my test to your branch");
+		System.out.println("Adrian, this is my next test to your branch");
 		menu.enter();
 	}
 }
