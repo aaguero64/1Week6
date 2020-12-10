e8d43252bc58b515d81fa11ee23e4ca40c39509b
Sri Srinivasan <sri.dhcs@gmail.com>
2020-12-10 13:07:22 -0800

    Second Test Line added

--- application/ApplicationCar.java
+++ application/ApplicationCar.java
@@ -4,6 +4,8 @@ public class ApplicationCar {
 	public static void main(String[] args) { // Main method
 		MenuCar menu = new MenuCar();
 		System.out.println("Test Line from Sri");
+		System.out.println("Second Test Line from Sri");
+
 		menu.enter();
 	}
 }
