2be1e9330c4dfb31eff034f880b67334e292164c
Sri Srinivasan <sri.dhcs@gmail.com>
2020-12-10 15:28:07 -0800

    2nd try to Adrian Branch-

--- application/ApplicationCar.java
+++ application/ApplicationCar.java
@@ -3,7 +3,7 @@ package application;
 public class ApplicationCar {
 	public static void main(String[] args) { // Main method
 		MenuCar menu = new MenuCar();
-		System.out.println("Adrian, this is my test to your branch);
+		System.out.println("Adrian, this is my test to your branch");
 		menu.enter();
 	}
 }
