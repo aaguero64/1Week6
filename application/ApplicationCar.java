63b99c22aafc74e2002ad295622c16738177e84f
Sri Srinivasan <sri.dhcs@gmail.com>
2020-12-10 16:07:02 -0800

    3 little changes

--- application/ApplicationCar.java
+++ application/ApplicationCar.java
@@ -3,8 +3,9 @@ package application;
 public class ApplicationCar {
 	public static void main(String[] args) { // Main method
 		MenuCar menu = new MenuCar();
-		System.out.println("Adrian, this is my test to your branch");
-		System.out.println("Adrian, this is my next test to your branch");
+//		System.out.println("Adrian, this is my test to your branch");
+//		System.out.println("Adrian, this is my next test to your branch");
 		menu.enter();
+		System.out.println("Thank you fo rusing the Car Rental Application");
 	}
 }
